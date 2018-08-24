c6ffd774e3a925c276750a144abb17c306f3dcd7
wjk <478286460@qq.com>
2018-08-24 22:17:38 +0800

    。

---
+++ Scripts/Unity/ScalePingpong.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalePingpong : MonoBehaviour
+{
+    public float timePingpongOnce = 0.55f;
+    public bool doWhenEnable = true;
+    public float maxScale = 1.5f;
+    float originScale;
+    bool on;
+    float timer;
+    void OnEnable()
+    {
+        originScale = transform.localScale.x;
+        if (doWhenEnable) on = true;
+    }
+    void Update()
+    {
+        if (!on) return;
+        timer += Time.deltaTime;
+        if (timer > timePingpongOnce)
+        {
+            if (timer > timePingpongOnce * 2)
+            {
+                transform.localScale = originScale * Vector3.one;
+                Destroy(this);
+                enabled = false;
+            }
+            else
+            {
+                transform.localScale = Vector3.one *
+                    Mathf.Lerp(originScale * maxScale, originScale, (timer - timePingpongOnce) / timePingpongOnce);
+            }
+        }
+        else
+        {
+            transform.localScale = Vector3.one *
+                Mathf.Lerp(originScale, originScale * maxScale, (timer / timePingpongOnce));
+        }
+    }
+}
---
+++ Scripts/Unity/ScalePingpong_UI_Text.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScalePingpong_UI_Text : MonoBehaviour
+{
+    public float timePingpongOnce = 0.2f;
+    public bool doWhenEnable = true;
+    public float maxScale = 1.2f;
+    int originSize;
+    bool on;
+    float timer;
+    Text text;
+    void OnEnable()
+    {
+        text = GetComponent<Text>();
+        originSize = text.fontSize;
+        if (doWhenEnable) on = true;
+    }
+    void Update()
+    {
+        if (!on) return;
+        timer += Time.deltaTime;
+        if (timer > timePingpongOnce)
+        {
+            if (timer > timePingpongOnce * 2)
+            {
+                text.fontSize = originSize;
+                Destroy(this);
+                enabled = false;
+            }
+            else
+            {
+                text.fontSize =Mathf.RoundToInt(
+                    Mathf.Lerp(originSize * maxScale, originSize, (timer - timePingpongOnce) / timePingpongOnce));
+            }
+        }
+        else
+        {
+            text.fontSize =Mathf.RoundToInt(
+                Mathf.Lerp(originSize, originSize * maxScale, (timer / timePingpongOnce)));
+        }
+    }
+}
