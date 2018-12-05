ee213bf50561499489250647202ce763e53ad4c1
wjk <478286460@qq.com>
2018-12-05 13:17:12 +0800

    。

--- Scripts/UI/Events.cs
+++ Scripts/UI/Events.cs
@@ -11,58 +11,6 @@ namespace Esa
         Right,
         Middle,
     };
-    public static class ICBTool
-    {
-        // 自动使用mono对象的名字和rectTransform
-        public static InputCallBack AddInputCB(this MonoBehaviour mono)
-        {            
-            return UI.UI.I.inputCallBacks.Add_R(new InputCallBack(mono, null, 0));
-        }
-        public static InputCallBack AddInputCB(this MonoBehaviour mono, int order)
-        {
-            return UI.UI.I.inputCallBacks.Add_R(new InputCallBack(mono, null, order));
-        }
-        public static InputCallBack AddInputCB(this MonoBehaviour mono, Action updateFunc, int order)
-        {
-            return UI.UI.I.inputCallBacks.Add_R(new InputCallBack(mono, updateFunc, order));
-        }
-        // 自定义名字，mono为null时rectTransform为null
-        public static InputCallBack AddInputCB(this MonoBehaviour mono, string name, Action updateFunc, int order)
-        {
-            return UI.UI.I.inputCallBacks.Add_R(new InputCallBack(name, updateFunc, order));
-        }
-    }
-    [Serializable]
-    public class InputCallBack
-    {
-        public InputCallBack() { }
-        /// <summary>
-        /// 降序
-        /// </summary>
-        public InputCallBack(MonoBehaviour mono, Action getInput, int order = 0)
-        {
-            this.mono = mono;
-            this.gameObject = mono.gameObject;
-            this.RT = mono.transform as RectTransform;
-            this.name = mono.name;
-            this.getInput = getInput;
-            this.order = order;
-        }
-        public InputCallBack(string name, Action getInput, int order = 0)
-        {
-            this.name = name;
-            this.getInput = getInput;
-            this.order = order;
-        }
-        public GameObject gameObject;
-        public RectTransform RT;
-        public Rt rt;
-        public MonoBehaviour mono;
-        public bool mouseOver;
-        public string name;
-        public Action getInput;
-        public int order;
-    }
     public static class Events
     {
         public static bool used;
