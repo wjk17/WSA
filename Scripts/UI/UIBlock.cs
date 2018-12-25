91dd7c91b8c0a2ca864e696494d701938d33a7bf
wjk <478286460@qq.com>
2018-12-25 21:51:21 +0800

    .

---
+++ Scripts/UI/UIBlock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Esa;
+using Esa.UI;
+public class UIBlock : Singleton<UIBlock>
+{
+    public override void _Awake()
+    {
+        base._Awake();
+        this.AddInput(-999);
+        var btn = this.GetComChild<Button>();
+        btn.onClick.AddListener(() =>
+        {
+            BlockUI(false);
+        });
+    }
+    public void BlockUI(bool block)
+    {
+        foreach (var mouse in TransTool.GetComsScene<MouseEventWrapper>())
+        {
+            mouse.enabled = !block;
+        }
+        foreach (var grid in TransTool.GetComsScene<UIGrid>())
+        {
+            grid.enabled = !block;
+        }
+        foreach (var row in TransTool.GetComsScene<Button_Row>())
+        {
+            row.enabled = !block;
+        }
+        foreach (var button in TransTool.GetComsScene<Button>())
+        {
+            if (button != this.GetComChild<Button>())
+                button.enabled = !block;
+        }
+        gameObject.SetActive(block);
+    }
+}
