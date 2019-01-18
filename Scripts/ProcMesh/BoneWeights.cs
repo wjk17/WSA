f2eba8c64bf6b156f864c0c99493e39031b30525
wjk <478286460@qq.com>
2019-01-18 21:59:19 +0800

    。

---
+++ Scripts/ProcMesh/BoneWeights.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+namespace Esa
+{
+    [Serializable]
+    public class BoneWeights
+    {
+        public BoneWeights() { wgts = new List<Weight>(); }
+        public List<Weight> wgts;
+        public static void NormalizeWeight(BoneWeights bws)
+        {
+            var weightTotal = 0f;
+            foreach (var w in bws.wgts)
+            {
+                weightTotal += w.weight;
+            }
+            var ratio = 1 / weightTotal;
+            foreach (var w in bws.wgts)
+            {
+                w.weight *= ratio;
+            }
+        }
+        public static Vector3 GeneratePos(BoneWeights bws)
+        {
+            var v = Vector3.zero;
+            foreach (Weight weight in bws.wgts)
+            {
+                v += weight.t.TransformPoint(weight.delta) * weight.weight;
+            }
+            return v;
+        }
+    }
+    [Serializable]
+    public class Weight
+    {
+        public Transform t;
+        public float weight;
+        public Vector3 delta;
+    }
+}
--- Scripts/Unity/ModelProcess/RematchSkinnedMeshBones.cs
+++ Scripts/Unity/ModelProcess/RematchSkinnedMeshBones.cs
@@ -1,24 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class RematchSkinnedMeshBones : MonoBehaviour
+namespace Esa
 {
-    public SkinnedMeshRenderer to;
-    public SkinnedMeshRenderer smr;
-    private void Reset()
+    public class RematchSkinnedMeshBones : MonoBehaviour
     {
-        to = transform.GetComponent<SkinnedMeshRenderer>();
-    }
-    [ContextMenu("Rematch")]
-    void Rematch()
-    {
-        //指定好新的rootbone，在新root下匹配名字相同的bone
-        var list = new List<Transform>();
-        foreach (var bone in smr.bones)
+        public SkinnedMeshRenderer to;
+        public SkinnedMeshRenderer smr;
+        private void Reset()
         {
-            list.Add(to.rootBone.Search(bone.name));
+            to = transform.GetComponent<SkinnedMeshRenderer>();
         }
-        to.bones = list.ToArray();
+        [ContextMenu("Rematch")]
+        void Rematch()
+        {
+            //指定好新的rootbone，在新root下匹配名字相同的bone
+            var list = new List<Transform>();
+            foreach (var bone in smr.bones)
+            {
+                list.Add(to.rootBone.Search(bone.name));
+            }
+            to.bones = list.ToArray();
+        }
     }
 }
